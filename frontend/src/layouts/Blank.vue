956b6b1486745946ae054a9d50e7b3ec14be5ab5
karmazin <kubartphil@gmail.com>
2021-05-17 22:21:08 +0300

    Blank layout set up

--- frontend/src/layouts/Blank.vue
+++ frontend/src/layouts/Blank.vue
@@ -1,36 +1,66 @@
 <template>
 <!-- App.vue -->
 <v-app>
-  <v-navigation-drawer v-model="drawer" app>
-    <!-- -->
+  <v-navigation-drawer v-model="drawer"
+        app
+        left
+        absolute
+        >
+      
+        <v-list-item two-line>
+          <v-list-item-avatar>
+            <img src="https://randomuser.me/api/portraits/women/81.jpg">
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title>Jane Smith</v-list-item-title>
+            <v-list-item-subtitle>Logged In</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
   </v-navigation-drawer>
 
-  <v-app-bar app color="blue accent-4 ">
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+  <v-app-bar app class="maincolor2">
+      <!-- color="blue accent-4 " -->
       <!-- https://material.io/resources/icons/?icon=3d_rotation&style=baseline -->
-      <v-toolbar-title>
+      <v-toolbar-title class="logo-space">
         <router-link to="/" teg="span">
-        
-          <span class="pointer">STORE APP</span>
+        <img 
+        class="logo-size"
+        src="../../public/target.svg"
+        ></router-link>
+        <router-link to="/" teg="span">
+        <span class="pointer">
+          <h5><strong>STORE APP</strong></h5>
+          
+          
+          
+          </span>
         </router-link>
       </v-toolbar-title>
+      <v-app-bar-nav-icon class="logo-size" @click="drawer = !drawer"></v-app-bar-nav-icon>
       
-      <v-app-bar-nav-icon @click="timeShow = !timeShow">
-        <span class="material-icons pointer">
-            access_time
-        </span>
-      </v-app-bar-nav-icon>
-    <v-app-bar-nav-icon @click="btnShow = !btnShow">
-        <span class="material-icons">
-            check_box_outline_blank
-            </span>
-      </v-app-bar-nav-icon>
-      <v-toolbar-title v-if="timeShow">
-        <span class="wight-class">{{date | date('datetime')}}</span>
-      </v-toolbar-title>
 
-      
       <v-spacer></v-spacer>
+      
+      
       <v-toolbar-items v-if="btnShow">
         <v-btn flat color="blue lighten-4">
           <v-icon left>bug_report</v-icon>
@@ -40,8 +70,7 @@
         <v-btn flat color="blue lighten-2">Link One</v-btn>
         <v-btn flat color="blue lighten-1">Link One</v-btn>
       </v-toolbar-items> 
-      <!-- <v-toolbar-items class="hidden-sm-and-down">
-      <v-toolbar-items>
+      <v-toolbar-items class="hidden-sm-and-down">
        <v-btn
           v-for="link in links"
           :key="link.title"
@@ -63,10 +92,10 @@
           Logout
         </v-btn> 
 
-      </v-toolbar-items>-->
+      </v-toolbar-items>
 
       <v-btn icon>
-        <!-- <v-icon>mdi-export</v-icon> -->
+        <v-icon>mdi-export</v-icon>
       </v-btn>
     </v-app-bar>
 
@@ -79,24 +108,7 @@
       <!-- If using vue-router -->
       <router-view></router-view>
     </v-container>
-    
-            
   </v-main>
-  <v-card-text style="height: 100px; position: relative" v-if="btnShow">
-<v-fab-transition>
-              <v-btn
-                v-show="!hidden"
-                color="pink"
-                dark
-                absolute
-                top
-                right
-                fab
-              >
-                <v-icon>mdi-plus</v-icon>
-              </v-btn>
-            </v-fab-transition>
-          </v-card-text>
   <v-footer 
 
     color="primary lighten-1"
@@ -105,10 +117,11 @@
   >
     <v-row
       justify="center"
-      no-gutters
+      no-gutter
+      class="maincolor"
     >
       <v-btn
-        v-for="link in links"
+        v-for="link in footerLinks"
         :key="link"
         color="white"
         text
@@ -118,11 +131,23 @@
         {{ link }}
       </v-btn>
       <v-col
-        class="primary lighten-2 py-4 text-center white--text"
+        class="maincolor py-4 text-center white--text"
         cols="12"
       >
-        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+      <!-- class="primary lighten-2 " -->
+      <p v-if="timeShow">&copy; {{date | date('datetime')}}  
+        <strong>STORE APP</strong>
+        <span class="material-icons  group pa-2" @click="timeShow = !timeShow">
+            <v-icon left>access_time</v-icon>
+        </span></p>
+        <p v-if="!timeShow">&copy; {{ new Date().getFullYear() }} 
+          <strong>STORE APP</strong>
+          <span class="material-icons group pa-2" @click="timeShow = !timeShow">
+            <v-icon left>access_time</v-icon>
+         </span>
+          </p>
       </v-col>
+      
     </v-row>
   </v-footer>
 </v-app>
@@ -130,7 +155,7 @@
 
 // <script>
 import messages from '../utils/messages'
-
+// import messages from '../../public/target.svg'
 
 export default  {
     name: 'blank-layout',
@@ -139,7 +164,7 @@ export default  {
       drawer: false,
       timeShow:true,
       btnShow:false,
-      links: [
+      footerLinks: [
         'Home',
         'About Us',
         'Team',
@@ -148,7 +173,12 @@ export default  {
         'Contact Us',
       ],
       date: new Date(),
-        };
+      items: [
+          { title: 'Home', icon: 'mdi-home-city' },
+          { title: 'My Account', icon: 'mdi-account' },
+          { title: 'Users', icon: 'mdi-account-group-outline' },
+        ],
+      } 
     },
     computed:{
         error () {
@@ -187,9 +217,28 @@ export default  {
 .pointer {
   cursor: pointer;
   color: white;
+  padding: 0 20px;
+}
+.pointer-f {
+  cursor: pointer;
+  color: white;
+  padding: 0 20px;
 }
 .wight-class{
    color: white; 
    padding: 0 10px;
    }
+.logo-size{
+  
+  height: 3rem;
+  width: auto;
+  margin: 5px 30px 5px 10px;
+  
+}
+.logo-space{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
 </style>
